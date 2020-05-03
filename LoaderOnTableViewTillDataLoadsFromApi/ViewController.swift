ef5fc35615212276979b3b800323a85ab556aa53
nikhilbalne <nikhil.b@ecoihub.com>
2020-05-03 20:48:17 +0530

    Initial Commit

---
+++ LoaderOnTableViewTillDataLoadsFromApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoaderOnTableViewTillDataLoadsFromApi
+//
+//  Created by Nikhil Balne on 03/05/20.
+//  Copyright © 2020 Nikhil Balne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
